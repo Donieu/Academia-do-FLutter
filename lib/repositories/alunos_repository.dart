27af12132212665bae199a196ec4af8d77e4f18b
Daniel Martins <dbreno09@gmail.com>
2022-12-02 11:16:17 -0300

    consertado erro de API

--- lib/repositories/alunos_repository.dart
+++ lib/repositories/alunos_repository.dart
@@ -6,8 +6,7 @@ import 'package:http/http.dart' as http;
 
 class AlunosRepository {
   Future<List<Aluno>> findAll() async {
-    final alunosResponse =
-        await http.get(Uri.parse('http://localhost:3031/alunos'));
+    final alunosResponse = await http.get(Uri.http('localhost:3031', '/alunos'));
     final alunosList = jsonDecode(alunosResponse.body);
     return alunosList.map<Aluno>((alunoMap) {
       return Aluno.fromMap(alunoMap);
@@ -15,7 +14,7 @@ class AlunosRepository {
   }
 
   Future<Aluno> findById(String id) async {
-    final alunoResponse = await http.get(Uri.parse('http://localhost:3031/alunos/'));
+    final alunoResponse = await http.get(Uri.http('localhost:3031', '/alunos/$id'));
     final alunoMap = jsonDecode(alunoResponse.body);
     return Aluno.fromMap(alunoMap);
     //return Aluno.fromJson(alunoResponse.body);
